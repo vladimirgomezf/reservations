ea479ef905016b9862f146352e30e9b57b10dd13
Vladimir Gómez Fumero <vladimirg.fumero@gmail.com>
2021-10-28 01:37:00 -0400

    feat: add the database.py file, db drive and crud methods

---
+++ backend/database.py
@@ -0,0 +1,33 @@
+from model import Reservation
+
+# MongoDB driver
+import motor.motor_asyncio
+
+client = motor.motor_asyncio.AsyncIOMotorClient('mongodb://localhost:27017/')
+database = client.Reservation
+collection = database.reservation
+
+async def fetch_one_reservation(phone):
+    document = await collection .find_one({"phone":phone})
+    return document
+
+async def fetch_all_reservations():
+    reservations = []
+    cursor = collection.find({})
+    async for document in cursor:
+        reservations.append(Reservations(**document))
+    return reservations
+
+async def create_reservation(Reservation):
+    document = reservation
+    result = await collection.insert_one(document)
+    return result
+
+async def update_todo(contact_name, contact_type, phone, birthdate, descriptions):
+    await collection.update_one({"contact_name":contact_name, "contact_type":contact_type, "phone":phone, "birthdate":birthdate, "descriptions":descriptions})
+    document = await collection.find_one({"phone":phone})
+    return document
+
+async def remove_reservation(phone):
+    await collection.delete_one({"phone":phone})
+    return True
